dcdcdbb5c36916aadaeab5529abb8cf969d21ced
DevLogsan <loganlegallou22@gmail.com>
2022-11-30 14:01:10 +0100

    Commit du nouveau projet

--- src/controleur/MainApp.java
+++ src/controleur/MainApp.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controleur;
 
 import java.io.IOException;
@@ -20,7 +16,7 @@ import modele.Session;
 
 /**
  *
- * @author Philippe
+ * @author EROS Philippe
  */
 public class MainApp extends Application
 {
